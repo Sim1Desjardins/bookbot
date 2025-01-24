61f712d7b773cd73e2ea7df0768cebb84b097b13
Sim1Desjardins <simonedesjardins7@gmail.com>
2025-01-24 11:11:20 -0500

    Word count

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def main():
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
-if __name__ == '__main__':
-    main()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
